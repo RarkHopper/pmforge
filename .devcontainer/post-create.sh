62b100dab1abe668300310091cc806b2b509f59b
Rark7040 <rarkhopper@gmail.com>
2025-03-31 11:52:27 +0000

    pmmpもインストールするように

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 set -e
 
-echo "Add command aliases to .bashrc..."
 bash .devcontainer/register-aliases.sh
 
-# Install pre-commit hooks
 pre-commit install
 
-# Composer setup (if you have a composer.json)
-if [ -f "composer.json" ]; then
-    composer install
-fi
-
-# PHP Intelephense setup
-code --install-extension bmewburn.vscode-intelephense-client --force
-
-echo "DevContainer setup complete!"
+composer install
